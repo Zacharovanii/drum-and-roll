aa3f973c3c19a7de885fead20a7f7f8bc2b5d64b
Zacharovanii <tuzudavid7@gmail.com>
2025-05-30 22:29:14 +0300

    aaaaaa

--- main.js
+++ main.js
@@ -144,3 +144,33 @@ document.getElementById("link-learning").addEventListener("click", () => {
 document.getElementById("link-pricing").addEventListener("click", () => {
 	smoothScrollTo("pricing");
 });
+
+const slides = document.querySelector(".slides");
+const slide = document.querySelectorAll(".slide");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentIndex = 0;
+const totalSlides = slide.length;
+
+function updateSliderPosition() {
+	slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+	if (currentIndex < totalSlides - 1) {
+		currentIndex++;
+	} else {
+		currentIndex = 0; // Возврат к первому слайду
+	}
+	updateSliderPosition();
+});
+
+prevBtn.addEventListener("click", () => {
+	if (currentIndex > 0) {
+		currentIndex--;
+	} else {
+		currentIndex = totalSlides - 1; // Переход на последний слайд
+	}
+	updateSliderPosition();
+});
